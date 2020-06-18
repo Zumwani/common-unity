ccb1b7a6452159de8af8183abdefde405eeddc89
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2020-06-18 17:36:31 +0200

    Update LerpUtility.cs

--- LerpUtility.cs
+++ LerpUtility.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace Common
 {
+
     public static class LerpUtility
     {
 
@@ -17,6 +18,50 @@ namespace Common
             {
 
                 callback?.Invoke(Mathf.Lerp(start, end, t));
+
+                time += Time.deltaTime;
+                t = time / duration;
+                yield return null;
+
+            }
+
+            callback?.Invoke(end);
+            onComplete?.Invoke();
+
+        }
+
+        public static IEnumerator Lerp(Vector3 start, Vector3 end, float duration, Action<Vector3> callback, Action onComplete = null)
+        {
+
+            var t = 0f;
+            var time = 0f;
+
+            while (t <= 1)
+            {
+
+                callback?.Invoke(Vector3.Lerp(start, end, t));
+
+                time += Time.deltaTime;
+                t = time / duration;
+                yield return null;
+
+            }
+
+            callback?.Invoke(end);
+            onComplete?.Invoke();
+
+        }
+
+        public static IEnumerator Lerp(Vector2 start, Vector2 end, float duration, Action<Vector2> callback, Action onComplete = null)
+        {
+
+            var t = 0f;
+            var time = 0f;
+
+            while (t <= 1)
+            {
+
+                callback?.Invoke(Vector2.Lerp(start, end, t));
 
                 time += Time.deltaTime;
                 t = time / duration;
