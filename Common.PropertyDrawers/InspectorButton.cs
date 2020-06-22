cb3d28b71e2177fe97d26fc2a227a9f284eed462
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2020-06-22 20:17:03 +0200

    Rewrote ButtonAttribute (now called InspectorButton) and CoroutineUtility to make them easier to use.

---
+++ Common.PropertyDrawers/InspectorButton.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System;
+using Common;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[Serializable]
+public struct InspectorButton
+{
+
+    public Action<object> action;
+
+    public static implicit operator InspectorButton(Action action) => Create(action);
+
+    public static InspectorButton Create(Action action) =>
+        new InspectorButton() { action = (caller) => action?.Invoke() };
+
+    public static InspectorButton Create<Parameter>(Parameter parameter, Action<Parameter> action) =>
+        new InspectorButton() { action = (caller) => action?.Invoke(parameter) };
+
+    public static InspectorButton Create<This>(Action<This> action) =>
+        new InspectorButton() { action = (caller) => action?.Invoke(GetThis<This>(caller)) };
+
+    public static InspectorButton Create<This, Parameter>(Parameter parameter, Action<This, Parameter> action) =>
+        new InspectorButton() { action = (caller) => action?.Invoke(GetThis<This>(caller), parameter) };
+
+    static T GetThis<T>(object caller) =>
+        typeof(T).IsAssignableFrom(caller?.GetType())
+        ? (T)caller
+        : default;
+
+    public void Invoke(object caller)
+    {
+        if (action != null)
+            action.Invoke(caller);
+        else
+        {
+            Debug.Log(caller);
+
+        }
+    }
+
+}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(InspectorButton))]
+public class ButtonDrawer : PropertyDrawer
+{
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        if (GUI.Button(position, label))
+        {
+            var action = (InspectorButton)property.GetValue();
+            action.Invoke(property.serializedObject.targetObject); 
+        }
+    }
+
+}
+#endif
--- CoroutineUtility.cs
+++ CoroutineUtility.cs
@@ -1,8 +1,13 @@
+#pragma warning disable IDE0062 // Make local function 'static': Unity does not support this...
+#pragma warning disable IDE0051 // Remove unused private members
+
 using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 using System;
 using Object = UnityEngine.Object;
+using System.Collections.Generic;
+using UnityEditor;
+using System.Threading.Tasks;
 
 namespace Common
 {
@@ -10,38 +15,193 @@ namespace Common
     public static class CoroutineUtility
     {
 
-        static readonly Dictionary<Coroutine, CoroutineHelper> coroutines = new Dictionary<Coroutine, CoroutineHelper>();
-
+        static CoroutineRoot root;
+        /// <summary>Runs the coroutine. This method can start any number of instances of this coroutine.</summary>
         public static Coroutine StartCoroutine(this IEnumerator coroutine, Action onComplete = null)
         {
 
-            var obj = new GameObject("Coroutine Runner");
+            if (coroutine == null)
+                return null;
+
+            if (!root)
+                root = new GameObject("Coroutine Runner").AddComponent<CoroutineRoot>();
+
+            var obj = new GameObject(coroutine.ToString().Replace("+<", ".").Replace(">", "()").Remove(coroutine.ToString().Length - 4, 4));
+            obj.transform.SetParent(root.transform);
             var runner = obj.AddComponent<CoroutineHelper>();
-            Coroutine c = null;
-            c = runner.StartCoroutine(Run());
+            var c = new Coroutine(coroutine, runner, onComplete);
+            runner.Run(coroutine, c);
 
             return c;
 
-            IEnumerator Run()
+        }
+           
+        [UnityEditor.Callbacks.DidReloadScripts]
+        static void OnScriptsReloaded()
+        {
+            var obj = Object.FindObjectsOfType<CoroutineRoot>();
+            foreach (var o in obj)
+                Object.DestroyImmediate(o.gameObject);
+        }
+
+        [ExecuteAlways]
+        class CoroutineRoot : MonoBehaviour
+        {
+
+#if UNITY_EDITOR
+
+            private void Start()
             {
-                yield return null;
-                coroutines.Add(c, runner);
-                yield return coroutine;
-                coroutines.Remove(c);
-                Object.Destroy(obj);
-                onComplete?.Invoke();
+                if (!Application.isPlaying)
+                    PushFramesInEditor();
+            }
+
+            void PushFramesInEditor()
+            {
+                EditorApplication.update -= EditorApplication.QueuePlayerLoopUpdate;
+                EditorApplication.update += EditorApplication.QueuePlayerLoopUpdate;
+                EditorApplication.QueuePlayerLoopUpdate();
+            }
+
+#endif
+
+            public void DestroyIfEmpty() =>
+                StartCoroutine(WaitAndDestroy(gameObject));
+                
+            public void Destroy(CoroutineHelper coroutine)
+            {
+                StartCoroutine(WaitAndDestroy(coroutine.gameObject));
+                StartCoroutine(WaitAndDestroy(gameObject, 2));
+            }
+               
+            IEnumerator WaitAndDestroy(GameObject obj, int frames = 1)
+            {
+                  
+                for (int i = 0; i < frames; i++)
+                    yield return null;
+
+                if (obj == gameObject && transform.childCount != 0)
+                    yield break;
+
+                if (Application.isPlaying)
+                    Destroy(obj);
+                else
+                    DestroyImmediate(obj);
+
             }
 
         }
 
-        public static void StopCoroutine(Coroutine coroutine)
+        static readonly Dictionary<string, Coroutine> coroutines = new Dictionary<string, Coroutine>();
+        /// <summary>Starts this coroutine. This method can only start a single instance of this coroutine.</summary>
+        public static void Start(this IEnumerator coroutine)
         {
-            if (coroutine != null && coroutines.TryGetValue(coroutine, out var runner))
-                Object.Destroy(runner);
+            if (coroutine == null)
+                return;
+            if (!coroutines.ContainsKey(coroutine.ToString()))
+                coroutines.Add(coroutine.ToString(), StartCoroutine(coroutine));
         }
 
+        /// <summary>Restarts this coroutine.</summary>
+        public static void Restart(this IEnumerator coroutine)
+        {
+            coroutine.Stop();
+            coroutine.Start();
+        }
+
+        /// <summary>Stops this coroutine.</summary>
+        public static void Stop(this IEnumerator coroutine) => coroutine?.GetActive()?.Stop();
+
+        /// <summary>Pauses this coroutine.</summary>
+        public static void Pause(this IEnumerator coroutine) => coroutine?.GetActive()?.Pause();
+
+        /// <summary>Resumes this coroutine, if paused.</summary>
+        public static void Resume(this IEnumerator coroutine) => coroutine?.GetActive()?.Resume();
+
+        /// <summary>Gets if this coroutine is currently running. Use IsPaused to check if the coroutine is paused.</summary>
+        public static bool IsRunning(this IEnumerator coroutine) => coroutine?.GetActive()?.IsRunning ?? false;
+
+        /// <summary>Gets if this coroutine is currently paused.</summary>
+        public static bool IsPaused(this IEnumerator coroutine) => coroutine?.GetActive() is Coroutine c && c.IsPaused && c.IsRunning;
+
+        /// <summary>Gets the currently active instance that was started from Start.</summary>
+        public static Coroutine GetActive(this IEnumerator coroutine)
+        {
+            if (coroutines.TryGetValue(coroutine?.ToString(), out var c))
+                return c;
+            else
+                return null;
+        }
+
+        public class Coroutine
+        {
+
+            public Coroutine(IEnumerator coroutine, CoroutineHelper helper, Action onComplete)
+            {
+                this.helper = helper;
+                this.coroutine = coroutine;
+                OnComplete = onComplete;
+            }
+
+            readonly IEnumerator coroutine;
+            readonly CoroutineHelper helper;
+
+            public Action OnComplete { get; private set; }
+            public bool IsPaused { get; private set; }
+            public bool IsRunning => helper;
+
+            public void Pause() => IsPaused = true;
+            public void Resume() => IsPaused = false;
+            public void Stop()
+            {
+                OnComplete?.Invoke();
+                coroutines.Remove(coroutine.ToString());
+                root.DestroyIfEmpty();
+                if (Application.isPlaying)
+                    Object.Destroy(helper.gameObject);
+                else
+                    Object.DestroyImmediate(helper.gameObject);
+            }
+
+        }
+          
+        [ExecuteAlways]
         public class CoroutineHelper : MonoBehaviour
-        { }
+        {
+                
+            public void Run(IEnumerator coroutine, Coroutine helper)
+            {
+                 
+                StartCoroutine(RunCoroutine(coroutine));
+
+                IEnumerator RunCoroutine(IEnumerator c)
+                {
+
+                    yield return RunSub(c);
+
+                    IEnumerator RunSub(IEnumerator sub)
+                    {
+                        while (sub.MoveNext())
+                        {
+
+                            while (helper.IsPaused)
+                                yield return null;
+
+                            if (sub.Current is IEnumerator sub1)
+                                yield return RunSub(sub1);
+
+                            yield return null;
+
+                        }
+                    }
+
+                    helper.Stop();
+
+                }
+
+            }
+
+        }
 
     }
 
